a83d7742c5845a604e7faa2cd3052b8edfebeebe
reannalim <reanna_lim@dlsu.edu.ph>
2016-03-14 21:19:39 +0800

    Delete inventory.sql

--- inventory.sql
+++
@@ -1,4 +0,0 @@
-SELECT rights.rightName FROM rights WHERE rights.rightId IN 
-(SELECT groupright.rightId FROM groupright 
-WHERE groupright.groupId = 1 AND groupright.active = 1) 
-AND rights.active = 1
